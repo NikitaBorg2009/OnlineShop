19e55605790a2e9a77b1649fad1415771eab5ea5
NikitaBorg2009 <NikitaBorg2009@gmail.com>
2025-11-04 13:18:37 +0300

    Updated AdminController

--- demo/src/main/java/ru/borzhemskiiNikita/demo/controllers/AdminController.java
+++ demo/src/main/java/ru/borzhemskiiNikita/demo/controllers/AdminController.java
@@ -15,6 +15,19 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+    /**
+     * Метод помогает изменить информацию старого продукта на новую
+     *
+     * @param oldProduct старый товар в виде {@link Product}
+     * @param name       имя нового продукта
+     * @param price      цена нового продукта
+     * @param rank       рейтинг нового продукта
+     * @param discount   скидка нового продукта
+     * @param group      категория или группа нового продукта
+     * @param count      кол-во нового продукта
+     * @param id         код нового продукта
+     * @return перекидывание на метод /accepted, если все проверки прошёл продукт, иначе перекидывание на метод /denied
+     */
     @PostMapping("/changeProduct")
     public String changeProductShop(@ModelAttribute("product") Product oldProduct, @RequestParam("1name") String name,
                                     @RequestParam("1price") int price, @RequestParam("1rank") int rank,
@@ -27,6 +40,12 @@ public class AdminController {
         return "redirect:/denied";
     }
 
+    /**
+     * Метод удаляет продукт из онлайн магазина
+     *
+     * @param product продукт который надо удалить в виде {@link Product}
+     * @return перекидывание на метод /accepted, если продукт удалился, иначе перекидывание на метод /denied
+     */
     @PostMapping("/deleteProduct")
     public String deleteProductSHOP(@ModelAttribute("product") Product product) {
         if (adminService.deleteProduct(product)) {
@@ -35,6 +54,12 @@ public class AdminController {
         return "redirect:/denied";
     }
 
+    /**
+     * Метод создаёт новый продукт или увеличивает кол-во продукта, который есть в магазине
+     *
+     * @param product продукт, который хотят создать или увеличить кол-во его
+     * @return перекидывание на метод /accepted, если продукт создался, иначе перекидывание на метод /denied
+     */
     @PostMapping("/createNewProduct")
     public String createNewProductShop(@ModelAttribute("product") Product product) {
         if (adminService.createNewProduct(product)) {
@@ -43,43 +68,79 @@ public class AdminController {
         return "redirect:/denied";
     }
 
+    /**
+     * Метод перекидывает администратора на страницу удаления товара из онлайн магазина
+     *
+     * @param model модель передаёт продукт, который надо удалить
+     * @return представление страницы deletePage
+     */
     @GetMapping("/getDeletePage")
     public String getDeletePage(Model model) {
         model.addAttribute("product", adminService.getProduct());
         return "deletePage";
     }
 
+    /**
+     * Метод перекидывает администратора на страницу изменения информации о товаре из онлайн магазина
+     *
+     * @param model модель передаёт продукт, который надо изменить
+     * @return представление страницы changePage
+     */
     @GetMapping("/getChangePage")
     public String getChangePage(Model model) {
         model.addAttribute("product", adminService.getProduct());
         return "changePage";
     }
 
+    /**
+     * Метод перекидывает администратора на страницу добавления продукта в онлайн магазин или увеличения его кол-ва
+     *
+     * @param model модель передаёт продукт, который надо добавить или увеличить его кол-во
+     * @return представление страницы addProductSHOP
+     */
     @GetMapping("/addProduct")
     public String getAddProductShop(Model model) {
         model.addAttribute("product", adminService.getProduct());
         return "addProductSHOP";
     }
 
+    /**
+     * Метод помогает сделать доставку бесплатной или платной (указать цену)
+     *
+     * @param choice выбор администратора (включить или выключить цену доставки)
+     * @return перекидывание на метод /accepted
+     */
     @PostMapping("/switchOnDelivery")
     public String switchOnDeliveryPrice(@RequestParam("choice") String choice) {
         adminService.switchOnDeliveryPrice(choice);
         return "redirect:/accepted";
     }
 
+    /**
+     * Метод перекидывает администратора на страницу включения или выключения доставки
+     *
+     * @param model модель передаёт информацию о доставки - включена или выключена она
+     * @return представление страницы switchOnDeliveryPage
+     */
     @GetMapping("/getSwitchOnDeliveryPage")
     public String switchOnDelivery(Model model) {
 
         if (adminService.switchOnDelivery()) {
             model.addAttribute("delivery", "Delivery is ON");
-        }
-        else {
+        } else {
             model.addAttribute("delivery", "Delivery is OFF");
         }
 
         return "switchOnDeliveryPage";
     }
 
+    /**
+     * Метод даёт возможность администратору изменить цену доставки
+     *
+     * @param money новая цена доставки
+     * @return перекидывание на метод /accepted, если цена доставки изменилась и прошла проверки, иначе перекидывание
+     * на метод /denied
+     */
     @PostMapping("/changeDeliveryPrice")
     public String changeDP(@RequestParam("money") int money) {
         if (adminService.changeDeliveryPrice(money)) {
@@ -88,11 +149,21 @@ public class AdminController {
         return "redirect:/denied";
     }
 
+    /**
+     * Метод перекидывает администратора на страницу изменения цены доставки
+     *
+     * @return представление страницы changeDeliveryPricePage
+     */
     @GetMapping("/getChangeDeliveryPricePage")
     public String getCPP() {
         return "changeDeliveryPricePage";
     }
 
+    /**
+     * Метод перекидывает администратора на меню админа
+     *
+     * @return представление страницы adminConsole
+     */
     @GetMapping("/adminConsole")
     public String getAC() {
         return "adminConsole";
